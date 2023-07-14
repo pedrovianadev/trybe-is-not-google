fe3eab4b2a46824292a1cb704900ba20e0e3f33d
Pedro Viana <pedroviana.developer13@gmail.com>
2023-07-14 14:59:21 -0300

    feat: function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+"""Linter"""
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    file = ".txt"
+    if file not in path_file:
+        print("Formato inválido", file=sys.stderr)
+        return None
+    try:
+        with open(path_file, mode="r", encoding="utf-8") as file_text:
+            line = file_text.read().split("\n")
+            return line
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
