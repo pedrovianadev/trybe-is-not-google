8551a1903f5aa21e3de4757545630ae537a73895
Pedro Viana <pedroviana.developer13@gmail.com>
2023-07-14 15:23:34 -0300

    feat: function remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 """Linter"""
+import sys
 from ting_file_management.file_management import txt_importer
 
 
@@ -12,9 +13,9 @@ def process(path_file, instance):
     txt_file = txt_importer(path_file)
 
     dictionary = {
-        'nome_do_arquivo': path_file,
-        'qtd_linhas': len(txt_file),
-        'linhas_do_arquivo': txt_file,
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_file),
+        "linhas_do_arquivo": txt_file,
     }
 
     instance.enqueue(dictionary)
@@ -24,6 +25,11 @@ def process(path_file, instance):
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos", file=sys.stdout)
+    file = instance.dequeue()
+    file_removed = file["nome_do_arquivo"]
+    print(f"Arquivo {file_removed} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
